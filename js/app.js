eb25a88ab8695029a7573d71e08860f8d77024bc
lollipop.fly@gmail.com <lollipop.fly@gmail.com>
2016-03-01 17:03:43 +0300

    add currency to localstorage

--- js/app.js
+++ js/app.js
@@ -4,3 +4,24 @@
 * Description
 */
 angular.module('app', []);
+
+var obj = [
+		{
+			id: 1,
+			code: "rub",
+			name: 'Рубль',
+			class: 'fa-rub'
+		},
+		{
+			id: 3,
+			code: 'dollar',
+			name: 'Доллар',
+			class: 'fa-usd'
+		},
+		{
+			id: 3,
+			code: 'euro',
+			name: 'Евро',
+			class: 'fa-eur'
+		}
+	];
--- js/controllers/currenciesCtrl.js
+++ js/controllers/currenciesCtrl.js
@@ -6,41 +6,40 @@ angular.module('app').controller('currenciesCtrl', ['$scope', 'currenciesFactory
 		$event.preventDefault();
 		if(typeof(Storage) !== "undefined") {
 			var storage = this.getStorageCurrencies(); // get currencies string from localstorage
-					// var currencySelectObj = JSON.parse(this.refs.currencySelect.value); // selected currency & make obj
 			// Если не пустая строка
 			if($scope.selectCurrency) {
-				console.log($scope.selectCurrency);
-			}
-					// if(storage) {
-					// 	// Chek the condition if selected currency is entry in localstorage
-					// 	for (var i = 0; i < storage.length; i++) {
-					// 		if(storage[i].name === currencySelectObj.name) {
-					// 			alert('Такая валюта уже есть!');
-					// 			return false;
-					// 		}
-					// 	}
-					// } else {
-					// 	//  localstorage is empty
-					// 	var storage = Array();
-					// }
+				var selectCurrency = JSON.parse($scope.selectCurrency);
 
-					// // Add currency
-					// storage.push(currencySelectObj);
-					// this.setState({storage: storage});
-					// storage = JSON.stringify(storage);
-					// localStorage.setItem('currencies', storage); // set currencies string to localstorage
-					$('#myModal').modal('hide'); // close modal
-				} else {
-					console.log('Sorry LocalStorage not working in this browser!');
+				if(storage) {
+					// Chek the condition if selected currency is entry in localstorage
+					for (var i = 0; i < storage.length; i++) {
+						if(storage[i].name === selectCurrency.name) {
+							alert('Такая валюта уже есть!');
+							return false;
+						}
+					}
 				}
+
+				// Add currency
+				storage.push(selectCurrency);
+				storage = JSON.stringify(storage);
+				localStorage.setItem('currencies', storage); // set currencies string to localstorage
+				storage = JSON.parse(storage);
+				$('#myModal').modal('hide'); // close modal
+			}
+
+		} else {
+			console.log('Sorry LocalStorage not working in this browser!');
+		}
 	};
 
 	this.getStorageCurrencies = function() {
+		var storage;
 		if(typeof(Storage) !== "undefined") {
-			var storage = localStorage.getItem("currencies");
-			if(storage) {
-				storage = JSON.parse(storage);
-			}
+			storage = localStorage.getItem("currencies");
+			storage = (storage) ? JSON.parse(storage) : [];
+		} else {
+			storage = false;
 		}
 
 		return storage;
